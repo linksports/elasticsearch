92a64a15ae5494b589829296cda18b3750d36074
momotaro <m.taro.773@gmail.com>
2025-11-11 15:38:00 +0900

    Fix type from interface{} to any

--- main.go
+++ main.go
@@ -50,25 +50,25 @@ const (
 type Document struct {
 	Index   string
 	ID      string
-	Body    interface{}
+	Body    any
 	Refresh RefreshPolicy
 }
 
 // for UpdateRequest
 type documentBody struct {
-	Doc interface{} `json:"doc"`
+	Doc any `json:"doc"`
 }
 
 type HitData struct {
-	Index string        `json:"_index"`
-	Id    string        `json:"_id"`
-	Score float64       `json:"_score"`
-	Sort  []interface{} `json:"sort"`
+	Index string  `json:"_index"`
+	Id    string  `json:"_id"`
+	Score float64 `json:"_score"`
+	Sort  []any   `json:"sort"`
 }
 
 type AggregationBucket struct {
-	Key      interface{} `json:"key"`
-	DocCount float64     `json:"doc_count"`
+	Key      any     `json:"key"`
+	DocCount float64 `json:"doc_count"`
 }
 
 type Elasticsearch interface {
@@ -81,8 +81,8 @@ type Elasticsearch interface {
 	UpdateDocument(doc *Document) (StatusCode, error)
 	RemoveDocument(doc *Document) (StatusCode, error)
 
-	Search(index string, query string, data interface{}) (StatusCode, []*HitData, int, error)
-	SearchWithAggregations(index string, query string, data interface{}, aggsKeys []string) (StatusCode, []*HitData, int, map[string][]*AggregationBucket, error)
+	Search(index string, query string, data any) (StatusCode, []*HitData, int, error)
+	SearchWithAggregations(index string, query string, data any, aggsKeys []string) (StatusCode, []*HitData, int, map[string][]*AggregationBucket, error)
 	GetSource(index string, id string, result any) (int, error)
 	Count(index string, query string) (StatusCode, int, error)
 
@@ -167,7 +167,7 @@ func (es *_elasticsearch) CreateDocument(doc *Document) (StatusCode, error) {
 		return StatusError, err
 	} else {
 		// Deserialize the response into a map.
-		var r map[string]interface{}
+		var r map[string]any
 		if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
 			log.Printf("Error parsing the response body: %s", err)
 			return StatusUnexpectedError, nil
@@ -222,18 +222,18 @@ func (es *_elasticsearch) CreateDocuments(docs []*Document) (StatusCode, error) 
 		return StatusError, err
 	} else {
 		// Deserialize the response into a map.
-		var r map[string]interface{}
+		var r map[string]any
 		if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
 			log.Printf("Error parsing the response body: %s", err)
 			return StatusUnexpectedError, nil
 		}
 
-		if items, ok := r["items"].([]interface{}); ok {
+		if items, ok := r["items"].([]any); ok {
 			for i, item := range items {
-				if itemMap, ok := item.(map[string]interface{}); ok {
+				if itemMap, ok := item.(map[string]any); ok {
 					for _, data := range itemMap {
-						if indexData, ok := data.(map[string]interface{}); ok {
-							if indexError, ok := indexData["error"].(map[string]interface{}); ok {
+						if indexData, ok := data.(map[string]any); ok {
+							if indexError, ok := indexData["error"].(map[string]any); ok {
 								log.Printf("[%d] error=%s,%s; id=%s", int(indexData["status"].(float64)), indexError["type"], indexError["reason"], indexData["_id"])
 							} else {
 								log.Printf("[%d] %s; version=%d ; id=%s", int(indexData["status"].(float64)), indexData["result"], int(indexData["_version"].(float64)), indexData["_id"])
@@ -292,7 +292,7 @@ func (es *_elasticsearch) UpdateDocument(doc *Document) (StatusCode, error) {
 		return StatusError, errors.New(res.String())
 	} else {
 		// Deserialize the response into a map.
-		var r map[string]interface{}
+		var r map[string]any
 		if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
 			log.Printf("Error parsing the response body: %s", err)
 			return StatusUnexpectedError, err
@@ -327,7 +327,7 @@ func (es *_elasticsearch) RemoveDocument(doc *Document) (StatusCode, error) {
 		return StatusError, errors.New(res.String())
 	} else {
 		// Deserialize the response into a map.
-		var r map[string]interface{}
+		var r map[string]any
 		if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
 			log.Printf("Error parsing the response body: %s", err)
 			return StatusUnexpectedError, errors.New("parse error")
@@ -337,7 +337,7 @@ func (es *_elasticsearch) RemoveDocument(doc *Document) (StatusCode, error) {
 	return StatusSuccess, nil
 }
 
-func (es *_elasticsearch) Search(index string, query string, data interface{}) (StatusCode, []*HitData, int, error) {
+func (es *_elasticsearch) Search(index string, query string, data any) (StatusCode, []*HitData, int, error) {
 	statusCode, result, err := search(es.client, index, query)
 	if err != nil {
 		return statusCode, []*HitData{}, 0, err
@@ -351,7 +351,7 @@ func (es *_elasticsearch) Search(index string, query string, data interface{}) (
 	return StatusSuccess, hitsData, total, nil
 }
 
-func (es *_elasticsearch) SearchWithAggregations(index string, query string, data interface{}, aggsKeys []string) (StatusCode, []*HitData, int, map[string][]*AggregationBucket, error) {
+func (es *_elasticsearch) SearchWithAggregations(index string, query string, data any, aggsKeys []string) (StatusCode, []*HitData, int, map[string][]*AggregationBucket, error) {
 	aggs := map[string][]*AggregationBucket{}
 	statusCode, result, err := search(es.client, index, query)
 	if err != nil {
@@ -369,16 +369,16 @@ func (es *_elasticsearch) SearchWithAggregations(index string, query string, dat
 
 	if len(aggsKeys) > 0 {
 		for _, key := range aggsKeys {
-			if _, ok := result["aggregations"].(map[string]interface{})[key]; !ok {
+			if _, ok := result["aggregations"].(map[string]any)[key]; !ok {
 				continue
 			}
 
-			buckets := result["aggregations"].(map[string]interface{})[key].(map[string]interface{})["buckets"].([]interface{})
+			buckets := result["aggregations"].(map[string]any)[key].(map[string]any)["buckets"].([]any)
 			var aggregationBucket []*AggregationBucket
 			for _, bucket := range buckets {
 				b := &AggregationBucket{
-					Key:      bucket.(map[string]interface{})["key"],
-					DocCount: bucket.(map[string]interface{})["doc_count"].(float64),
+					Key:      bucket.(map[string]any)["key"],
+					DocCount: bucket.(map[string]any)["doc_count"].(float64),
 				}
 				aggregationBucket = append(aggregationBucket, b)
 			}
@@ -454,8 +454,8 @@ func connectElasticsearch(config *Config) *goElasticsearch.Client {
 	return client
 }
 
-func search(client *goElasticsearch.Client, index string, query string) (StatusCode, map[string]interface{}, error) {
-	var result map[string]interface{}
+func search(client *goElasticsearch.Client, index string, query string) (StatusCode, map[string]any, error) {
+	var result map[string]any
 	res, err := client.Search(
 		client.Search.WithContext(context.Background()),
 		client.Search.WithIndex(index),
@@ -471,15 +471,15 @@ func search(client *goElasticsearch.Client, index string, query string) (StatusC
 
 	if res.IsError() {
 		var esErr error
-		var e map[string]interface{}
+		var e map[string]any
 		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
 			esErr = fmt.Errorf("error parsing the response body: %s", err)
 		} else {
 			//Print the response status and error information.
 			esErr = fmt.Errorf("[%s] %s: %s",
 				res.Status(),
-				e["error"].(map[string]interface{})["type"],
-				e["error"].(map[string]interface{})["reason"],
+				e["error"].(map[string]any)["type"],
+				e["error"].(map[string]any)["reason"],
 			)
 		}
 		log.Println(esErr)
@@ -498,35 +498,35 @@ func search(client *goElasticsearch.Client, index string, query string) (StatusC
 	return StatusSuccess, result, nil
 }
 
-func parseHitsData(result map[string]interface{}, data interface{}) (StatusCode, []*HitData, int, error) {
+func parseHitsData(result map[string]any, data any) (StatusCode, []*HitData, int, error) {
 	if _, ok := result["hits"]; !ok {
 		return StatusNoContent, []*HitData{}, 0, nil
 	}
 
 	total := 0
-	if t, existsTotal := result["hits"].(map[string]interface{})["total"]; existsTotal {
-		total = int(t.(map[string]interface{})["value"].(float64))
+	if t, existsTotal := result["hits"].(map[string]any)["total"]; existsTotal {
+		total = int(t.(map[string]any)["value"].(float64))
 	}
 
-	hits := result["hits"].(map[string]interface{})["hits"].([]interface{})
+	hits := result["hits"].(map[string]any)["hits"].([]any)
 
-	documents := make([]interface{}, len(hits))
+	documents := make([]any, len(hits))
 	hitsData := make([]*HitData, len(hits))
 
 	for i, hit := range hits {
-		documents[i] = hit.(map[string]interface{})["_source"]
+		documents[i] = hit.(map[string]any)["_source"]
 
 		h := &HitData{
-			Index: hit.(map[string]interface{})["_index"].(string),
-			Id:    hit.(map[string]interface{})["_id"].(string),
+			Index: hit.(map[string]any)["_index"].(string),
+			Id:    hit.(map[string]any)["_id"].(string),
 		}
 
-		if score := hit.(map[string]interface{})["_score"]; score != nil {
+		if score := hit.(map[string]any)["_score"]; score != nil {
 			h.Score = score.(float64)
 		}
 
-		if sort := hit.(map[string]interface{})["sort"]; sort != nil {
-			h.Sort = sort.([]interface{})
+		if sort := hit.(map[string]any)["sort"]; sort != nil {
+			h.Sort = sort.([]any)
 		}
 
 		hitsData[i] = h
